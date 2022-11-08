dcdbdd1d25693243801d6c33c4839fa48e266e50
alanbc <alanbc60@gmail.com>
2022-11-08 09:36:40 -0600

    implementacion de sockets udp

---
+++ implemntacionSockets/src/main.rs
@@ -0,0 +1,26 @@
+use std::net::UdpSocket;
+use std::thread;
+use std::str;
+
+fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:34254")?;
+    let mut buf = [0; 2048];
+
+    loop {
+      match socket.recv_from(&mut buf) {
+        Ok((buf_size, src_addr)) => {
+          thread::spawn(move || {
+            let buf = &mut buf[..buf_size];
+            let req_msg = str::from_utf8(&buf).unwrap();
+            println!("{:}", "=".repeat(80));
+            println!("buffer size: {:?}", buf_size);
+            println!("src address: {:?}", src_addr);
+            println!("request message: {:?}", req_msg);
+          });
+        },
+        Err(e) => {
+          println!("couldn't recieve request: {:?}", e);
+        }
+      }
+    }
+}
