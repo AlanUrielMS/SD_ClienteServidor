dd945971fe4f56a2cddf6689069dbe669f6f66df
AlanUrielMS <116377472+AlanUrielMS@users.noreply.github.com>
2022-11-17 22:19:04 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::io::*;
 use std::net::UdpSocket;
 use std::str;
 use std::str::from_utf8;
+use std::str::FromStr;
 
 
 
@@ -82,43 +83,96 @@ fn main(){
 	let listen_addr = net::SocketAddrV4::new(ip, 1425);
 	let send_addr = net::SocketAddrV4::new(ip, 1426);
 	
+
+
+    println!("DESPLEGANDO MENU");
+    println!("1) Consulta de id ");
+    println!("2) Consulta por informe problema ");
+    println!("3) Consulta por nombre reporte");
+    println!("Que tipo de consulta desea realizar?: ");
     //==Seccion: Lo que le enviaremos al servidor==
-
-    //Entrada de texto del println
-	let mut entrada_consulta = String::new();
-    println!("Consulta--Productos que empiezan con la letra: ");
-
     //Leer por consola
-	let guardarEntradaTexto = std::io::stdin().read_line(&mut entrada_consulta);
+    let mut entrada_seleccion = String::new();
+    std::io::stdin().read_line(&mut entrada_seleccion).ok().expect("Error al leer de teclado");
+    let seleccion: i32 = i32::from_str(&entrada_seleccion.trim()).unwrap();
+    //let mut entrada_consultat = String::new();
+    
+    if seleccion == 1{
+        println!("Consulta por id- ingresa el id");
+        //Entrada de texto del println
+	    println!("Ingrese el id a buscar: ");
+        let mut entrada_id = String::new();
+        let guardarEntradaTexto = std::io::stdin().read_line(&mut entrada_id);
+        //Se acorta el string para evitar la lectuira del salto de linea
+        let entrada_id = &entrada_id[0..1];
+        
+        //Se guarda en un vector
+	    let data: Vec<u8> = entrada_id.as_bytes().to_vec();
+	    enviarMensaje(net::SocketAddr::V4(send_addr), net::SocketAddr::V4(listen_addr), data);
 
-    //Se guarda en un vector
-	let data: Vec<u8> = entrada_consulta.as_bytes().to_vec();
+        //==Seccion: Respuesta del servidor==//
+	    let msg = listen(net::SocketAddr::V4(send_addr));
+	    println!("mensaje recibido de cliente");
+        
 
-	enviarMensaje(net::SocketAddr::V4(send_addr), net::SocketAddr::V4(listen_addr), data);
-	//println!("{:}", "=".repeat(80));
+    
+        // Mostrar los productos que empiecen con m
+        let mut repo = Repository::new();
+        let products = repo.consulta_id(&msg);
+        print!("========== Productos que empiezan con M =========");
+        for item in products{
+            println!("| {} | {:<10} |", item.id, item.nombreReporte);
+        }
+    
+    }else if seleccion == 2{
+        println!("Consulta de informe del problema, ingrese un carater(r=red,s=sistema)");
+        //Entrada de texto del println
+	    let mut entrada_consulta = String::new();
+        println!("Ingrese el tipo de reporte a buscar: ");
+        //Leer por consola
+	    let guardarEntradaTexto = std::io::stdin().read_line(&mut entrada_consulta);
+        //Se acorta el string para evitar la lectuira del salto de linea
+        let entrada_consulta = &entrada_consulta[0..1];
+        //Se guarda en un vector
+	    let data: Vec<u8> = entrada_consulta.as_bytes().to_vec();
+	    enviarMensaje(net::SocketAddr::V4(send_addr), net::SocketAddr::V4(listen_addr), data);
+        //==Seccion: Respuesta del servidor==//
+	    let msg = listen(net::SocketAddr::V4(send_addr));
+	    println!("mensaje recibido de cliente");
+    
+        // Mostrar los productos que empiecen con m
+        let mut repo = Repository::new();
+        let products = repo.consulta_informe(&msg);
+        print!("========== Productos que empiezan con M =========");
+        for item in products{
+            println!("| {} | {:<10} |", item.id, item.informeProblema);
+        }
 
-
-    //==Seccion: Respuesta del servidor==//
-	let msg = listen(net::SocketAddr::V4(send_addr));
-	println!("mensaje recibido: {:?}", msg);
-	println!("{:}", "=".repeat(80));
-
-    // Mostrar los productos que empiecen con m
-    let mut repo = Repository::new();
-    let products = repo.products_start_withid(&msg);
-    print!("========== Productos que empiezan con M =========");
-    for item in products{
-        println!("| {} | {:<10} |", item.id, item.name);
+    }else if seleccion == 3{
+        println!("Consulta reporte  por nombre: ");
+        //Entrada de texto del println
+	    let mut entrada_consulta = String::new();
+        //Leer por consola
+	    let guardarEntradaTexto = std::io::stdin().read_line(&mut entrada_consulta);
+        //Se acorta el string para evitar la lectuira del salto de linea
+        let entrada_consulta = &entrada_consulta[0..1];
+        //Se guarda en un vector
+	    let data: Vec<u8> = entrada_consulta.as_bytes().to_vec();
+	    enviarMensaje(net::SocketAddr::V4(send_addr), net::SocketAddr::V4(listen_addr), data);
+        
+        //==Seccion: Respuesta del servidor==//
+	    let msg = listen(net::SocketAddr::V4(send_addr));
+	    println!("mensaje recibido de cliente");
+    
+        // Mostrar los productos que empiecen con m
+        let mut repo = Repository::new();
+        let products = repo.consulta_por_nombre(&msg);
+        print!("========== Productos que empiezan con M =========");
+        for item in products{
+            println!("| {} | {:<10} |", item.id, item.nombreReporte);
+        }
     }
-    //======fin del socket
 
-
-    // let products = repo.all_products();
-    // println!("========== ALL PRODUCTS =========");
-    // for item in products{
-    //     println!("| {} | {:<10} |", item.id, item.name);
-    // }
-
-
+    
     println!("Fin del programa");
 }
